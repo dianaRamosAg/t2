c2fe5d5a3a9efa864b39f15905224195e1bde12f
Diana Laura Ramos Aguikar <52227360+dianaRamosAg@users.noreply.github.com>
2019-07-08 20:43:21 -0600

    Initial commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,116 @@
+const http = require('http');
+const path = require('path');
+const status = require('http-status');
+const jwt = require('jsonwebtoken');
+const _config = require('../_config');
+
+let _user;
+
+const createUser = (req, res) => {
+    const user = req.body;
+    _user.create(user)
+        .then((data) => {
+            res.status(200);
+            res.json({ msg: "Usuario creado correctamente", data: data });
+        })
+        .catch((err) => {
+            res.status(400);
+            res.json({ msg: "Error!!!!", data: err });
+        })
+}
+
+const findAll = (req, res) => {
+    _user.find()
+        .then((data) => {
+            if (data.length == 0) {
+                res.status(status.NO_CONTENT);
+                res.json({ msg: "No se encontraron usuarios" });
+            } else {
+                res.status(status.OK);
+                res.json({ msg: "Éxito!!!", data: data });
+            }
+        })
+        .catch((err) => {
+            res.status(status.BAD_REQUEST);
+            res.json({ msg: "Error!!!" });
+        });
+}
+
+const deleteByID = (req, res) => {
+    const { id } = req.params;
+    // const id = req.params.id;
+
+    const params = {
+        _id: id
+    };
+
+    _user.findByIdAndRemove(params)
+        .then((data) => {
+            res.status(status.OK);
+            res.json({ msg: "Éxito!!!", data: data });
+        })
+        .catch((err) => {
+            res.status(status.NOT_FOUND);
+            res.json({ msg: "Error!!! No se encontró", err: err });
+        });
+
+}
+
+const login = (req, res) => {
+    const { email, password } = req.params;
+    let query = { email: email, password: password };
+    _user.findOne(query, "-password")
+        .then((user) => {
+            if (user) {
+                const token = jwt.sign({ email: email }, _config.SECRETJWT);
+                res.status(status.OK);
+                res.json({
+                    msg: "Acceso exitoso",
+                    data: {
+                        user: user,
+                        token: token
+                    }
+                });
+            } else {
+                res.status(status.NOT_FOUND);
+                res.json({ msg: "Error!!! No se encontró" });
+            }
+        })
+        .catch((err) => {
+            res.status(status.NOT_FOUND);
+            res.json({ msg: "Error!!! No se encontró", err: err });
+        });
+};
+const csvFilePath='C:\\Users\\Jaquelin Ramos A\\Desktop\\Diana\\ae2019vu3eje01-master\\usuarios.csv'
+const csv=require('csvtojson');
+
+const csvUsuarios = (req, res) => {
+     csv()
+    .fromFile(csvFilePath)
+    .then((jsonObj)=>{
+        console.log(jsonObj);
+
+        _user.create(jsonObj)
+        .then((jsonObj)=> {
+            res.json({msg:"Se han creado todos los usuarios", jsonObj: jsonObj});}
+        )
+        .catch((err)=> {
+            res.status(status.BAD_REQUEST);
+            res.json({msg:"Error", data:err});
+        })
+    }).catch((err) => {
+        res.json({msg:"Error", data:err});
+    })
+  //  const jsonArray=await csv().fromFile(csvFilePath);
+}
+
+module.exports = (User) => {
+    _user = User;
+    return ({
+        createUser,
+        findAll,
+        deleteByID,
+        login,
+        csvUsuarios
+    });
+}
